11ec2b45b0389ddafcf6de5f3877498a33226c5d
Richa <138646246+rita-kri@users.noreply.github.com>
2023-07-10 19:56:37 +0530

    Update BlogApplicationApiApplicationTests.java

--- src/test/java/com/example/demo/BlogApplicationApiApplicationTests.java
+++ src/test/java/com/example/demo/BlogApplicationApiApplicationTests.java
@@ -10,4 +10,19 @@ class BlogApplicationApiApplicationTests {
 	void contextLoads() {
 	}
 
+
+
+
+	//How to push the project first time
+	//create repo in github
+	//go to project folder and open with git base
+	//git init
+	//git status
+	//git add .
+	//git commit -m "ur commit"
+	//git branch -M main
+	// git remote add origin git@github.com:rita-kri/test.git
+	//git push -u origin main
+
+
 }
